ad4864fe84d0360483752309578f51e11e60c938
Sergio Juanes Tébar <70594373+SergioJuanes@users.noreply.github.com>
2021-07-12 20:18:49 +0200

    Update update.R

--- update.R
+++ update.R
@@ -97,51 +97,7 @@ if(!is.null(pdf_file)){
   
   write_csv(spain_covid, "data/spain_covid_dataset.csv")
 }
-                                        
-                                        
-                                        
-fecha <- Sys.Date() 
-pdf_file <- tryCatch(
-  pdf_text(paste0("https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_GIV_comunicacion_", str_replace_all(as.Date(fecha), "-", ""), ".pdf")),
-  error=function(e) NULL
-)
-if (!is.null(pdf_file)){
-  datos_unlist <- unlist(str_split(pdf_file[3], "[\\r\\n]+"))
-  datos_unlist <- data.frame(str_split_fixed(str_trim(datos_unlist), "\\s{2,}", 11))
-  datos_comunidades <- data.frame(apply(datos_unlist, 2, function(x) gsub("^$", NA, trimws(x))))
-  datos_comunidades$X11 <- NULL
-  datos_comunidades <- datos_comunidades %>% filter(complete.cases(datos_comunidades))
-  names(datos_comunidades) <- c("Comunidad", "Pfizer", "Moderna", "AstraZeneca", "Janssen", "Entregadas", "Administradas", "PercAdministradas", "Personas1Dosis", "Personas2Dosis")
-  
-  datos_comunidades <- data.frame(lapply(datos_comunidades, as.character), stringsAsFactors=FALSE)
-  datos_comunidades <- data.frame(lapply(datos_comunidades, str_replace_all, pattern = "\\.", replacement = ""))
-  datos_comunidades <- data.frame(lapply(datos_comunidades, str_replace_all, pattern = "%", replacement = ""))
-  datos_comunidades <- data.frame(lapply(datos_comunidades, str_replace_all, pattern = ",", replacement = "\\."))
-  datos_comunidades <- data.frame(lapply(datos_comunidades, as.character), stringsAsFactors=FALSE)
-  datos_comunidades[2:10] <- data.frame(lapply(datos_comunidades[2:10], as.numeric))
-  datos_comunidades$Comunidad <- c("Andalucía", "Aragón", "Principado de Asturias", "Islas Baleares", "Islas Canarias", "Cantabria", "Castilla y León", "Castilla-La Mancha" , "Cataluña", "Comunidad Valenciana", "Extremadura", "Galicia", "La Rioja", "Comunidad de Madrid", "Región de Murcia", "Comunidad Foral de Navarra", "País Vasco", "Ceuta", "Melilla", "Fuerzas Armadas", "Sanidad Exterior", "España")
-  datos_comunidades$fecha <- as.Date(fecha)
-  vacunacion <- datos_comunidades
-  vacunacion <- vacunacion[-which(vacunacion$Comunidad == "Fuerzas Armadas"),]
-  vacunacion <- vacunacion[-which(vacunacion$Comunidad == "Sanidad Exterior"),]
-  rownames(vacunacion) <- NULL
-  pobcoms <- data.frame(Comunidad = c("Andalucía", "Aragón", "Principado de Asturias", "Islas Baleares", "Islas Canarias", "Cantabria", "Castilla y León", "Castilla-La Mancha" , "Cataluña", "Comunidad Valenciana", "Extremadura", "Galicia", "La Rioja", "Comunidad de Madrid", "Región de Murcia", "Comunidad Foral de Navarra", "País Vasco", "Ceuta", "Melilla"), stringsAsFactors = FALSE, Poblacion = c(8464411, 1329391, 1018784, 1171543, 2175952, 582905, 2394918, 2045221, 7780479, 5057353, 1063987, 2701819, 319914, 6779888, 1511251, 661197, 2220504, 84202, 87076))
-  
-  pobcoms <- rbind(pobcoms, data.frame(Comunidad = "España", Poblacion = sum(pobcoms$Poblacion)))
-  
-  vacunacion <- left_join(vacunacion, pobcoms, by = "Comunidad")
-  vacunacion$percvacunados <- round(100*vacunacion$Personas2Dosis/vacunacion$Poblacion, 2)
-  
-  spain_vac <- read.csv("data/vacspain.csv", stringsAsFactors = FALSE, encoding = "UTF-8")
-  spain_vac$fecha <- as.Date(spain_vac$fecha)
-  spain_vac <- rbind(spain_vac, vacunacion)
-  
-  write_csv(spain_vac, "data/vacspain.csv")
-}
-
-                                        
-                                        
-                                     
+                                                                
 
 uci_covid_spain <- read_csv("data/ucispain.csv")
 number_file <- max(uci_covid_spain$number_file) + 1
